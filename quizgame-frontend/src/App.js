4c0f2bd10e476358e15976b74271871d2ddc3eba
raphkoch <s0582442@htw-berlin.de>
2023-05-23 20:52:08 +0200

    dark mode toggle -> only accessible if logged in

--- quizgame-frontend/src/App.js
+++ quizgame-frontend/src/App.js
@@ -23,6 +23,7 @@ function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [showHelp, setShowHelp] = useState(false);
   const [helpContent, setHelpContent] = useState('');
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
   const client = useMemo(() => new DefaultApi(), []);
 
@@ -153,144 +154,170 @@ function App() {
     }
   };
 
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
+  const showDarkModeToggle = isLoggedIn;
+
   return (
-    <div className="auth-container">
-      {!isLoggedIn ? (
-        <div className="auth-form">
-          <h2>{isRegistering ? 'Register' : 'Login'}</h2>
-          <input
-            type="text"
-            placeholder="Username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className={isRegistering && errorMessage && password !== confirmPassword ? 'error' : ''}
-          />
-          {isRegistering && (
-            <>
-              <input
-                type="password"
-                placeholder="Confirm Password"
-                value={confirmPassword}
-                onChange={(e) => setConfirmPassword(e.target.value)}
-                className={errorMessage && password !== confirmPassword ? 'error' : ''}
-              />
-              {password !== confirmPassword && (
-                <p className="error-message">Passwords do not match</p>
-              )}
-              <input
-                type="text"
-                placeholder="Email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </>
-          )}
-          {errorMessage && !isRegistering && (
-            <p className="error-message">{errorMessage}</p>
-          )}
-          <button onClick={isRegistering ? handleRegister : handleLogin}>
-            {isRegistering ? 'Register' : 'Login'}
-          </button>
-          {!isRegistering && (
-            <p>
-              Don't have an account yet?{' '}
-              <a href="#" onClick={() => setIsRegistering(true)}>
-                Register!
-              </a>
-            </p>
-          )}
-          {isRegistering && (
-            <p>
-              Already have an account?{' '}
-              <a href="#" onClick={handleSwitchToLogin}>
-                Back to Login
-              </a>
-            </p>
-          )}
-        </div>
-      ) : (
-        <>
-          <div className="game-container">
-            {isLoading ? (
-              <div className="loading-spinner">
-                <div className="spinner"></div>
-              </div>
-            ) : (
+    <div className={`App ${isDarkMode ? 'dark-mode' : ''}`}>
+      <div className="auth-container">
+        {!isLoggedIn ? (
+          <div className="auth-form">
+            <h2>{isRegistering ? 'Register' : 'Login'}</h2>
+            <input
+              type="text"
+              placeholder="Username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className={isRegistering && errorMessage && password !== confirmPassword ? 'error' : ''}
+            />
+            {isRegistering && (
               <>
-                {!isAnswered ? (
-                  <>
-                    <h2>Question</h2>
-                    <p>{currentQuestion.question}</p>
-                    {currentQuestion.answerChoices.map((choice, index) => (
-                      <button key={index} onClick={() => handleAnswer(choice)}>
-                        {choice}
-                      </button>
-                    ))}
-                  </>
-                ) : (
-                  <div className="feedback-container">
-                    <p className={isCorrect ? 'feedback-correct' : 'feedback-incorrect'}>
-                      {isCorrect ? 'Correct!' : 'Incorrect!'}
-                    </p>
-                    <button onClick={handleNextQuestion}>Next Question</button>
-                  </div>
+                <input
+                  type="password"
+                  placeholder="Confirm Password"
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                  className={errorMessage && password !== confirmPassword ? 'error' : ''}
+                />
+                {password !== confirmPassword && (
+                  <p className="error-message">Passwords do not match</p>
                 )}
-                <div className="score-container">
-                  <p>Score: {score}</p>
-                </div>
-                <div className="progress-container">
-                  <p>Question {gameSession.questionNumber} of {gameSession.totalQuestions}</p>
-                </div>
+                <input
+                  type="text"
+                  placeholder="Email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
               </>
             )}
+            {errorMessage && !isRegistering && (
+              <p className="error-message">{errorMessage}</p>
+            )}
+            <button onClick={isRegistering ? handleRegister : handleLogin}>
+              {isRegistering ? 'Register' : 'Login'}
+            </button>
+            {!isRegistering && (
+              <p>
+                Don't have an account yet?{' '}
+                <a href="#" onClick={() => setIsRegistering(true)}>
+                  Register!
+                </a>
+              </p>
+            )}
+            {isRegistering && (
+              <p>
+                Already have an account?{' '}
+                <a href="#" onClick={handleSwitchToLogin}>
+                  Back to Login
+                </a>
+              </p>
+            )}
+            {showDarkModeToggle && (
+              <div className="dark-mode-toggle" onClick={toggleDarkMode}>
+                {isDarkMode ? (
+                  <span className="toggle-icon">☀️</span>
+                ) : (
+                  <span className="toggle-icon">🌙</span>
+                )}
+              </div>
+            )}
           </div>
-          <div className="settings-container">
-            <h3>Settings</h3>
-            <div className="difficulty-levels">
-              <label>Difficulty Level:</label>
-              <select value={difficulty} onChange={handleDifficultyChange}>
-                {difficultyLevels.map((level, index) => (
-                  <option key={index} value={level}>{level}</option>
-                ))}
-              </select>
+        ) : (
+          <>
+            <div className="game-container">
+              {isLoading ? (
+                <div className="loading-spinner">
+                  <div className="spinner"></div>
+                </div>
+              ) : (
+                <>
+                  {!isAnswered ? (
+                    <>
+                      <h2>Question</h2>
+                      <p>{currentQuestion.question}</p>
+                      {currentQuestion.answerChoices.map((choice, index) => (
+                        <button key={index} onClick={() => handleAnswer(choice)}>
+                          {choice}
+                        </button>
+                      ))}
+                    </>
+                  ) : (
+                    <div className="feedback-container">
+                      <p className={isCorrect ? 'feedback-correct' : 'feedback-incorrect'}>
+                        {isCorrect ? 'Correct!' : 'Incorrect!'}
+                      </p>
+                      <button onClick={handleNextQuestion}>Next Question</button>
+                    </div>
+                  )}
+                  <div className="score-container">
+                    <p>Score: {score}</p>
+                  </div>
+                  <div className="progress-container">
+                    <p>Question {gameSession.questionNumber} of {gameSession.totalQuestions}</p>
+                  </div>
+                </>
+              )}
             </div>
-            <div className="category-selection">
-              <label>Select Category:</label>
-              <select value={selectedCategory} onChange={handleCategoryChange}>
-                <option value="">All Categories</option>
-                {categories.map((category, index) => (
-                  <option key={index} value={category}>{category}</option>
-                ))}
-              </select>
-            </div>
-            <button onClick={handleLogout}>Logout</button>
-            <div className="help-container">
-              <span className="help-icon" onClick={toggleHelp}>
-                ?
-              </span>
-              {showHelp && (
-                <div className="help-tooltip">
-                  <h4>Game Rules</h4>
-                  <p>Here are the game rules:</p>
-                  <ul>
-                    <li>Answer the questions correctly to earn points.</li>
-                    <li>The difficulty level determines the complexity of the questions.</li>
-                    <li>Select a category to narrow down the question topics.</li>
-                    <li>You can view your score and progress on the screen.</li>
-                  </ul>
-                  <p>Enjoy the game and have fun!</p>
+            <div className="settings-container">
+              <h3>Settings</h3>
+              <div className="difficulty-levels">
+                <label>Difficulty Level:</label>
+                <select value={difficulty} onChange={handleDifficultyChange}>
+                  {difficultyLevels.map((level, index) => (
+                    <option key={index} value={level}>{level}</option>
+                  ))}
+                </select>
+              </div>
+              <div className="category-selection">
+                <label>Select Category:</label>
+                <select value={selectedCategory} onChange={handleCategoryChange}>
+                  <option value="">All Categories</option>
+                  {categories.map((category, index) => (
+                    <option key={index} value={category}>{category}</option>
+                  ))}
+                </select>
+              </div>
+              <button onClick={handleLogout}>Logout</button>
+              <div className="help-container">
+                <span className="help-icon" onClick={toggleHelp}>
+                  ?
+                </span>
+                {showHelp && (
+                  <div className="help-tooltip">
+                    <h4>Game Rules</h4>
+                    <p>Here are the game rules:</p>
+                    <ul>
+                      <li>Answer the questions correctly to earn points.</li>
+                      <li>The difficulty level determines the complexity of the questions.</li>
+                      <li>Select a category to narrow down the question topics.</li>
+                      <li>You can view your score and progress on the screen.</li>
+                    </ul>
+                    <p>Enjoy the game and have fun!</p>
+                  </div>
+                )}
+              </div>
+              {showDarkModeToggle && (
+                <div className="dark-mode-toggle" onClick={toggleDarkMode}>
+                  {isDarkMode ? (
+                    <span className="toggle-icon">☀️</span>
+                  ) : (
+                    <span className="toggle-icon">🌙</span>
+                  )}
                 </div>
               )}
             </div>
-          </div>
-        </>
-      )}
+          </>
+        )}
+      </div>
     </div>
   );
 }
