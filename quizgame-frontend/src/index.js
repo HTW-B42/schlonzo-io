19a66623ed120793bf1a598fddda0ac9d80987eb
DoenertellerVersace <jakob.hadulla@gmail.com>
2023-05-30 16:49:52 +0200

    react with routes

--- quizgame-frontend/src/index.js
+++ quizgame-frontend/src/index.js
@@ -4,7 +4,7 @@ import Login from './Login.js';
 import {BrowserRouter as Router, Link, Route, Routes} from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 import ReactDOM from 'react-dom/client';
-import Home from "./home";
+import Home from "./Home";
 
 
 const state = {
