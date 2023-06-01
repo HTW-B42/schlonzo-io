c0af81f73578a58e755319d3fd690c9713f8ae53
helmo <102999393+helmotw@users.noreply.github.com>
2023-06-01 23:58:48 +0200

    Christopher (#47)
    * proto q-controller
    
    * delete q test

--- quizgame-server/src/main/java/org/htw/quizgame/server/data/QuestionRepository.java
+++ quizgame-server/src/main/java/org/htw/quizgame/server/data/QuestionRepository.java
@@ -2,6 +2,10 @@ package org.htw.quizgame.server.data;
 
 import org.htw.quizgame.server.model.Question;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import java.util.List;
+import java.util.Optional;
 
 public interface QuestionRepository extends MongoRepository<Question, String> {
+    Optional<Question> findQuestionByQuestion(String s);
+    void deleteQuestionByQuestion(String s);
 }
