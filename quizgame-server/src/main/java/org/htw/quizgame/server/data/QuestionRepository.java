8537e50e6b0f5e6b20775b11c98a6bf2b9197375
s0582080 <102999393+helmotw@users.noreply.github.com>
2023-06-03 23:17:54 +0200

    delete query

--- quizgame-server/src/main/java/org/htw/quizgame/server/data/QuestionRepository.java
+++ quizgame-server/src/main/java/org/htw/quizgame/server/data/QuestionRepository.java
@@ -2,10 +2,15 @@ package org.htw.quizgame.server.data;
 
 import org.htw.quizgame.server.model.Question;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
 import java.util.List;
 import java.util.Optional;
 
 public interface QuestionRepository extends MongoRepository<Question, String> {
+    @Query("{ 'question' : { $regex: ?0, $options: 'i' } }")
     Optional<Question> findQuestionByQuestion(String s);
+
+    @Query("{ 'question' : { $regex: ?0, $options: 'i' } }")
     void deleteQuestionByQuestion(String s);
 }
