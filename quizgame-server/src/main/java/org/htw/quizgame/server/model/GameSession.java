585700e334540c19f177ab1496edba646be9f59a
DoenertellerVersace <jakob.hadulla@gmail.com>
2023-06-19 18:37:04 +0200

    weiter

--- quizgame-server/src/main/java/org/htw/quizgame/server/model/GameSession.java
+++ quizgame-server/src/main/java/org/htw/quizgame/server/model/GameSession.java
@@ -1,7 +1,11 @@
 package org.htw.quizgame.server.model;
 
 
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Generated;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.htw.quizgame.api.model.GameSessionDTO;
 import org.htw.quizgame.server.model.util.ConvertsTo;
 import org.htw.quizgame.server.model.util.SaveAs;
@@ -16,21 +20,21 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Getter
 public class GameSession implements SaveAs<GameResult>, ConvertsTo<GameSessionDTO> {
 
-  private static final Collector<UserScore, ?, Map<User, Boolean>> TO_ANSWERS_MAP =
-      Collectors.toMap(UserScore::getUser, ignored -> false);
-  private static final Collector<UserScore, ?, Map<User, BigDecimal>> TO_SCORE_MAP =
-      Collectors.toMap(UserScore::getUser, UserScore::getScore);
+//  private static final Collector<UserScore, ?, Map<User, Boolean>> TO_ANSWERS_MAP =
+//      Collectors.toMap(UserScore::getUser, ignored -> false);
+//  private static final Collector<UserScore, ?, Map<User, BigDecimal>> TO_SCORE_MAP =
+//      Collectors.toMap(UserScore::getUser, UserScore::getScore);
+  private final String gameID = UUID.randomUUID().toString();
   private final List<UserSession> lobbyMembers = new ArrayList<>();
   private final List<UserScore> scoreboard = new ArrayList<>();
-  private final LinkedHashMap<Question, Map<User, Boolean>> questions = new LinkedHashMap<>();
-  private Question question;
-  private Map<User, Boolean> answers;
-  @Id
-  private String gameID;
+//  private final LinkedHashMap<Question, Map<User, Boolean>> questions = new LinkedHashMap<>();
+//  private Question question = null;
+//  private Map<User, Boolean> answers = new HashMap<>();
   private Boolean gameOver = false;
 
   private Optional<UserScore> getScoreFor(User user) {
@@ -45,33 +49,33 @@ public class GameSession implements SaveAs<GameResult>, ConvertsTo<GameSessionDT
     }
     initializeMember(userSession);
   }
+//
+//  public void removeMember(UserSession userSession) {
+//    if (gameOver) {
+//      return;
+//    }
+//    lobbyMembers.remove(userSession);
+//  }
 
-  public void removeMember(UserSession userSession) {
-    if (gameOver) {
-      return;
-    }
-    lobbyMembers.remove(userSession);
-  }
+//  public GameSession answerQuestion(User user, boolean correct) {
+//    answers.put(user, correct);
+//    return this;
+//  }
 
-  public GameSession answerQuestion(User user, boolean correct) {
-    answers.put(user, correct);
-    return this;
-  }
-
-  public GameSession nextQuestion(Question question) {
-    if (this.question != null) {
-      questions.put(this.question, this.answers);
-    }
-    if (question == null) {
-      // TODO end game from service
-      end();
-      return this;
-    }
-    this.question = question;
-    this.answers = new HashMap<>(scoreboard.stream().collect(TO_ANSWERS_MAP));
-    questions.put(question, null);
-    return this;
-  }
+//  public GameSession nextQuestion(Question question) {
+//    if (this.question != null) {
+//      questions.put(this.question, this.answers);
+//    }
+//    if (question == null) {
+//       TODO end game from service
+//      end();
+//      return this;
+//    }
+//    this.question = question;
+//    this.answers = new HashMap<>(scoreboard.stream().collect(TO_ANSWERS_MAP));
+//    questions.put(question, null);
+//    return this;
+//  }
 
   private void end() {
     gameOver = true;
@@ -94,7 +98,8 @@ public class GameSession implements SaveAs<GameResult>, ConvertsTo<GameSessionDT
 
   @Override
   public GameResult toEntity() {
-    return new GameResult(scoreboard.stream().collect(TO_SCORE_MAP), questions);
+//    return new GameResult(scoreboard.stream().collect(TO_SCORE_MAP), questions);
+    return null;
   }
 
   private void initializeMember(UserSession userSession) {
--- quizgame-server/src/main/java/org/htw/quizgame/server/model/Question.java
+++ quizgame-server/src/main/java/org/htw/quizgame/server/model/Question.java
@@ -1,15 +1,38 @@
 package org.htw.quizgame.server.model;
 
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.htw.quizgame.api.model.QuestionDTO;
 import org.htw.quizgame.server.model.util.ConvertsTo;
 import org.htw.quizgame.server.model.util.SaveAs;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.util.List;
-
+@Getter
 @Document
-public record Question(String question, List<String> answers, String correctAnswer)
-    implements ConvertsTo<QuestionDTO>, SaveAs<Question> {
+@NoArgsConstructor
+@AllArgsConstructor
+public class Question implements ConvertsTo<QuestionDTO>, SaveAs<Question> {
+
+  @Id
+  private String id;
+
+  private String question;
+
+  private List<String> answers;
+
+  private String correctAnswer;
+
+  public Question(QuestionDTO dto) {
+    super();
+    question = dto.getQuestion();
+    answers = dto.getAnswerChoices();
+    correctAnswer = dto.getCorrectAnswer();
+  }
+
+
   @Override
   public QuestionDTO toDTO() {
     return new QuestionDTO()
@@ -21,5 +44,15 @@ public record Question(String question, List<String> answers, String correctAnsw
   @Override
   public Question toEntity() {
     return this;
+  }
+
+  @Override
+  public String toString() {
+    return "Question{" +
+        "Id='" + id + '\'' +
+        ", question='" + question + '\'' +
+        ", answers=" + answers +
+        ", correctAnswer='" + correctAnswer + '\'' +
+        '}';
   }
 }
